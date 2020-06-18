8c3b5393aa94939b2a46dd25b8c1d69ae5f26d56
wanni-yang <712715156@qq.com>
2020-06-18 14:59:59 +0800

    zuoye

--- week07/css/css-standards..js
+++ week07/css/css-standards..js
@@ -1,15 +1,17 @@
-var lis = document.getElementById("container").children
+// https://www.w3.org/TR/?tag=css
+var lis = document.getElementById("container").children // 1223条
 
 var result = [];
 
 for (let li of lis) {
     if (li.getAttribute('data-tag').match(/css/))
         result.push({
-            name: li.children[1].innerText,
-            url: li.children[1].children[0].href
+            name: li.children[1].innerText, // 标题
+            url: li.children[1].children[0].href // 链接
         })
 }
-console.log(result)
+console.log(JSON.stringify(result, null, 4))
+console.log('W3C有'+result.length+'条css标准') // 139条
 
 
 let iframe = document.createElement('iframe');
