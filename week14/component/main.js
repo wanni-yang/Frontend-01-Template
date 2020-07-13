857f89088eae5db81f9831897d02d98617cd5f84
wanni-yang <712715156@qq.com>
2020-07-13 23:11:50 +0800

    作业

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,43 +1,143 @@
 // import "./foo";
-function create(Cls, attributes,...children){
+// 框架代码 将jsx写法翻译为用户代码
+function create(Cls, attributes, ...children) {
     // 
-    let o = new Cls({
-        timer:{}
-    });
-    for(let name in attributes){
+    console.log("create Cls: ", Cls)
+    let o;
+    if (typeof Cls === "string") {
+        o = new Wrapper(Cls)
+    } else {
+        o = new Cls({
+            timer: {}
+        });
+    }
+
+    for (let name in attributes) {
         // attribute和property一样
         // o[name] = attributes[name]
         // attribute和property不等效
         o.setAttribute(name, attributes[name])
     }
     // console.log(children)
-    for(let child of children){
+    for (let child of children) {
+        console.log(child, typeof child)
+        if (typeof child === "string")
+            child = new Text(child)
         o.appendChild(child)
     }
     return o;
 }
-class Parent{
-    constructor(config){
-        console.log("config: ",config)
+class Text {
+    constructor(text) {
+        this.child = [];
+        this.root = document.createTextNode(text)
     }
-    set class(v){//property
-        console.log("parent::class",v)
-
-    }
-    setAttribute(name, value){//attribute
-        console.log('attribute: ',name,value)
-    }
-    appendChild(child){//children
-        console.log("Parent::appendchild: ",child)
+    mountTo(parent) {
+        parent.appendChild(this.root);
     }
 }
-class Child{}
-class Cls{}
-let component = <Parent id='a' class="b">
-        <Child></Child>
-        <Child></Child>
-        <Child></Child>
-    </Parent>;
+class Wrapper {
+    constructor(type) {
+        this.children = [];
+        this.root = document.createElement(type);
+    }
+    // property 写法 和setAttribute取一
+    // set class(v){//property
+    //     console.log("parent::class",v)
+
+    // }
+    // set id(v){//property
+    //     console.log("parent::id", id)
+    // }
+    setAttribute(name, value) {//attribute
+        this.root.setAttribute(name, value)
+        console.log('attribute: ', name, value)
+    }
+    appendChild(child) {//children
+        // child.mountTo(this.root)
+        // console.log("Parent::appendchild: ",child)
+        this.children.push(child)
+    }
+    // 生命周期
+    mountTo(parent) {
+        parent.appendChild(this.root);
+        for (let child of this.children) {
+            child.mountTo(this.root)
+        }
+    }
+}
+// 用户代码
+class MyComponent {
+    constructor(config) {
+        console.log("config: ", config)
+        this.children = [];
+        this.root = document.createElement("div");
+    }
+    // property 写法 和setAttribute取一
+    // set class(v){//property
+    //     console.log("parent::class",v)
+
+    // }
+    // set id(v){//property
+    //     console.log("parent::id", id)
+    // }
+    setAttribute(name, value) {//attribute
+        this.root.setAttribute(name, value)
+        console.log('attribute: ', name, value)
+    }
+    appendChild(child) {//children
+        // child.mountTo(this.root)
+        // console.log("Parent::appendchild: ",child)
+        this.children.push(child)
+        // this.slot.appendChild(child)
+    }
+    render() {
+        return <article>
+            <header>lalala</header>
+            {this.slot}
+            <footer>end</footer></article>
+    }
+    // 生命周期
+    mountTo(parent) {
+
+        this.slot = <div></div>
+        // parent.appendChild(this.root);
+        for (let child of this.children) {
+            this.slot.appendChild(child)
+            // child.mountTo(this.slot)
+        }
+        this.render().mountTo(parent)
+    }
+}
+// class Child{
+//     constructor(config){
+//         this,children = [];
+//         this.root = document.createElement("div");
+//     }
+
+//     setAttribute(name, value){//attribute
+//         this.root.setAttribute(name, value)
+//     }
+//     appendChild(child){//children
+//         this.children.push(child)
+//     }
+//     // 生命周期
+//     mountTo(parent){
+//         parent.appendChild(this.root);
+//         for(let child of children){
+//             child.mountTo(this.root)
+//         }
+//     }
+// }
+// class Cls{}
+// parent child都是div的情况，可以将parent和child都变为div,同时可以删去class child
+// let component = <div id='a' class="b" style = "width:100px;height:100px;background-color:lightgreen">
+//         <div></div>
+// <div></div>
+// <p>{new Wrapper('span')}</p>
+//         <div></div>
+//     </div>;
+let component = <MyComponent><div>test</div></MyComponent>
 // 语法糖
 // var component = create(Parent,{
 //     id:"a",
@@ -54,4 +154,6 @@ let component = <Parent id='a' class="b">
 //     id:'a'
 // })
 // component.setAttribute('id','a')
-component.class = 'vv'
+// property
+// component.class = 'vv';
+component.mountTo(document.body)
