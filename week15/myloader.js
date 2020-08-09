8eec66ad62532e6539fcbee3d91d25beb3d52cd2
wanni-yang <712715156@qq.com>
2020-08-09 21:53:04 +0800

    作业

--- week15/myloader.js
+++ week15/myloader.js
@@ -1,7 +1,48 @@
-const { module } = require("./webpack.config");
+// const { module } = require("./webpack.config");
+var parser = require("./parser");
 
 module.exports = function(source, map){
-    console.log(source)
-    console.log("my loader is running \n",this.resourcePath)
-    return '';
+    let tree = parser.parseHTML(source);
+    // console.log(tree.children[1].children[0].content)
+    let template = null;
+    let script = null;
+    for(let node of tree.children){
+        if(node.tagName == 'template'){
+            template = node.children.filter(e => e.type != 'text')[0];
+        }
+        if(node.tagName == 'script'){
+            script = node.children[0].content;
+        }
+    }
+    let createCode = "";
+    
+    let visit = (node) =>{
+        if(node.type === 'text'){
+            return JSON.stringify(node.content)
+        }
+        let attrs = {};
+        for(let attribute of node.attributes){
+            attrs[attribute.name] = attribute.value;
+        }
+        let children = node.children.map(node=>visit(node)).join(',');
+        return `create("${node.tagName}",${JSON.stringify(attrs)},${children})`
+    }
+    
+    let targrt = `
+import { create, Text, Wrapper } from './create'
+export class Carousel {
+    setAttribute(name, value) { //attribute
+        this[name] = value;
+    }
+    render(){
+        return ${visit(template)};
+    }
+    mountTo(parent) {
+        this.render().mountTo(parent)
+    }
+    
+}
+    `;
+    console.log(targrt)
+    return targrt
 }
