d92e9cb543e515dd073e14cc88e2f4bf4244e75f
wanni-yang <712715156@qq.com>
2020-11-04 17:33:22 +0800

    animation

---
+++ week15/animation/animation.js
@@ -0,0 +1,22 @@
+import { time } from "console";
+
+export class Timeline{}
+export class Animation{}
+
+/*let animation = new Animation(object, property, start, end, duration, delay, timingFunction);
+let animation2 = new Animation(object2, property2, start, end, duration, delay, timingFunction);
+
+let timeline = new Timeline;
+
+timeline.add(animation)
+timeline.add(animation2)
+
+timeline.start()
+timeline.pause()
+timeline.resume()
+timeline.stop()
+
+setTimeout
+setInterval
+requestAnimationFrame
+*/
